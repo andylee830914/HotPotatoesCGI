ac89a375d0ac03e505de1ed36866b445c8c65020
andylee830914 <andylee0914.tw@gmail.com>
2014-05-08 11:10:25 +0800

    ignore null post

--- test.php
+++ test.php
@@ -1,18 +1,18 @@
-<script language="javascript">setTimeout("self.opener = null; self.close();",5000)</script>
+<script language="javascript">setTimeout("self.opener = null; self.close();",2000)</script>
 <?php
 header('Content-Type: text/html; charset=utf-8');
-$realname = $_POST['realname'];
-$Exercise = $_POST['Exercise'];
+$realname = iconv("big5","utf-8",$_POST['realname']);
+$Exercise = iconv("big5","utf-8",$_POST['Exercise']);
 $Score = $_POST['Score'];
-$Start_Time = $_POST['Start_Time'];
-$End_Time = $_POST['End_Time'];
+$Start_Time = iconv("big5","utf-8",$_POST['Start_Time']);
+$End_Time = iconv("big5","utf-8",$_POST['End_Time']);
 // Connect to the local database using username root and no password.
-$link=mysql_connect("localhost", "root", "ilovetaiwan1010");
+$link=mysql_connect("localhost", "root", "sqlpasswd");
 // Select our database named scores.
-mysql_select_db (scores);
+mysql_select_db(scores);
 mysql_set_charset('utf8',$link);
 // insert our 5 fields into the table eng101
-mysql_query ("insert into test
+if ($_POST['Score']){mysql_query ("insert into test
 (
 realname,
 Exercise,
@@ -29,6 +29,7 @@ VALUES
 '$End_Time'
 )
 ");
+};
 //Provide some feedback to the student.
 echo ("Your answers have been saved!");
 ?>
