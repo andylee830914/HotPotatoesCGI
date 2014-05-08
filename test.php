6438c1bf7000ba62a047a348745b1e4a8dba7023
andylee830914 <andylee0914.tw@gmail.com>
2014-05-08 23:29:12 +0800

    save to database and ignore null data

---
+++ test.php
@@ -0,0 +1,35 @@
+<script language="javascript">setTimeout("self.opener = null; self.close();",2000)</script>
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$realname = iconv("big5","utf-8",$_POST['realname']);
+$Exercise = iconv("big5","utf-8",$_POST['Exercise']);
+$Score = $_POST['Score'];
+$Start_Time = iconv("big5","utf-8",$_POST['Start_Time']);
+$End_Time = iconv("big5","utf-8",$_POST['End_Time']);
+// Connect to the mysql.
+$link=mysql_connect("localhost", "root", "sqlpasswd");
+// Select your database named scores.
+mysql_select_db(scores);
+mysql_set_charset('utf8',$link);
+// insert our 5 fields into the table test
+if ($_POST['Score']){mysql_query ("insert into test
+(
+realname,
+Exercise,
+Score,
+Start_Time,
+End_Time
+)
+VALUES
+(
+'$realname',
+'$Exercise',
+'$Score',
+'$Start_Time',
+'$End_Time'
+)
+");
+};
+//Provide some feedback to the student.
+echo ("Your answers have been saved!");
+?>
